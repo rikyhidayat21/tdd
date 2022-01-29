3531ad830a1dc7f980ce562d0b625df1d02ae084
rikyhidayat21 <rikyhidayat21@gmail.com>
2022-01-29 20:20:17 +0700

    finishing custom matcher

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Board do
   describe '#total_pieces' do
     it 'has a total of 32 pieces' do
       b = Board.new
-      expect(b.total_pieces).to CustomMatcher.custom_matcher
+      expect(b.total_pieces).to CustomMatcher.custom_matcher(32)
     end
   end
 end
--- spec/custom_matcher.rb
+++ spec/custom_matcher.rb
@@ -1,9 +1,20 @@
 module CustomMatcher
   class OurCustomMatcher
+    
+    def initialize(target)
+      @target = target
+    end
 
+    def matches?(value)
+      value == @target
+    end
+
+    def failure_message
+      "Our matcher failed to match"
+    end
   end
 
-  def self.custom_matcher
-    OurCustomMatcher.new
+  def self.custom_matcher(target)
+    OurCustomMatcher.new(target)
   end
 end
