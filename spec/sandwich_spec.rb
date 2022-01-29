ffbd07aaf30d4748d75a25a2cf8865ef4fbd46fd
rikyhidayat21 <rikyhidayat21@gmail.com>
2022-01-29 21:07:46 +0700

    setup before hooks

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -18,10 +18,19 @@ describe Sandwich do
   end
 
   context 'when the sandwich should not be vegan' do
-    it 'should have meat'
+    it 'should have meat' do
+      sandwich = Sandwich.new(true, true, ['lettuce', 'tomato', 'mayonaise'])
+      expect(sandwich.meat).to be(true)
+    end
 
-    it 'should have cheese'
+    it 'should have cheese' do
+      sandwich = Sandwich.new(true, true, ['lettuce', 'tomato', 'mayonaise'])
+      expect(sandwich.cheese).to be(true)
+    end
 
-    it 'should have mayonaise'
+    it 'should have mayonaise' do
+      sandwich = Sandwich.new(true, true, ['lettuce', 'tomato', 'mayonaise'])
+      expect(sandwich.condiments).to include('mayonaise')
+    end
   end
 end
