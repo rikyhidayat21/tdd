ac683da22c2897fc43d9e677722ee33e813fc5fb
rikyhidayat21 <rikyhidayat21@gmail.com>
2022-01-29 20:59:59 +0700

    initialize Sandwich class, but not implemented let rspec

--- lib/sandwich.rb
+++ lib/sandwich.rb
@@ -1,3 +1,9 @@
 class Sandwich
-  
+  attr_accessor :meat, :cheese, :condiments
+
+  def initialize(meat, cheese, condiments=[])
+    self.meat = meat
+    self.cheese = cheese
+    self.condiments = condiments
+  end
 end
--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -2,11 +2,20 @@ require_relative '../lib/sandwich.rb'
 
 describe Sandwich do
   context 'when the sandwich should be vegan' do
-    it 'should not have meat'
+    it 'should not have meat' do
+      sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
+      expect(sandwich.meat).to eq(false)
+    end
 
-    it 'should not have cheese'
+    it 'should not have cheese' do
+      sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
+      expect(sandwich.cheese).to eq(false)
+    end
 
-    it 'should not have mayonaise'
+    it 'should not have mayonaise' do
+      sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
+      expect(sandwich.condiments).to_not include('mayonaise')
+    end
   end
 
   context 'when the sandwich should not be vegan' do
