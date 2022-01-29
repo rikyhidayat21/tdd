319600009146e95dc5bc71cd5693fe7ab049d7bb
rikyhidayat21 <rikyhidayat21@gmail.com>
2022-01-29 21:01:29 +0700

    create let hooks

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,19 +1,18 @@
 require_relative '../lib/sandwich.rb'
 
 describe Sandwich do
+  let(:sandwich) { Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard']) }
+  
   context 'when the sandwich should be vegan' do
     it 'should not have meat' do
-      sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
       expect(sandwich.meat).to eq(false)
     end
 
     it 'should not have cheese' do
-      sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
       expect(sandwich.cheese).to eq(false)
     end
 
     it 'should not have mayonaise' do
-      sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
       expect(sandwich.condiments).to_not include('mayonaise')
     end
   end
